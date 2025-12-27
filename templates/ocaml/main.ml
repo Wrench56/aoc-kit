e8b88bdeeda86e35b12ddebea1fbcbd768cfab0f
Mark Devenyi <markdevenyidev@gmail.com>
2025-12-27 01:13:57 -0500

    fix(templates): align `input.txt` handling and `.gitignore` with new layout

--- templates/ocaml/main.ml
+++ templates/ocaml/main.ml
@@ -9,7 +9,7 @@ let report f input partnum =
 ;;
 
 let () =
-  let file = if Array.length Sys.argv > 1 then Sys.argv.(1) else "input.txt" in
+  let file = if Array.length Sys.argv > 1 then Sys.argv.(1) else "../input.txt" in
   let input = read file in
   if report Solution.part1 input 1 |> String.length > 0
   then report Solution.part2 input 2 |> ignore

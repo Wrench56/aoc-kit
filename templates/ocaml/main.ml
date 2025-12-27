a5595e9746cf625f0c6579267064a93a567c13c3
Mark Devenyi <markdevenyidev@gmail.com>
2025-12-27 00:44:59 -0500

    feat(templates): add `ocaml` template

---
+++ templates/ocaml/main.ml
@@ -0,0 +1,16 @@
+let read file = In_channel.with_open_text file In_channel.input_lines
+
+let report f input partnum =
+  let start = Unix.gettimeofday () in
+  let outp = f input in
+  let stop = Unix.gettimeofday () in
+  Printf.printf "Part%d executed in %.4fs:\n%s\n" partnum (stop -. start) outp;
+  outp
+;;
+
+let () =
+  let file = if Array.length Sys.argv > 1 then Sys.argv.(1) else "input.txt" in
+  let input = read file in
+  if report Solution.part1 input 1 |> String.length > 0
+  then report Solution.part2 input 2 |> ignore
+;;
---
+++ templates/ocaml/solution.ml
@@ -0,0 +1,9 @@
+let part1 input =
+  ignore input;
+  ""
+;;
+
+let part2 input =
+  ignore input;
+  ""
+;;
